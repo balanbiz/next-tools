bf406dd869e44eef3f579df1e40245109ea16d8e
Vlad <balanbiz@yandex.ru>
2023-09-25 14:47:31 +0300

    pagination + next updated

--- src/Components/PaginationControls.tsx
+++ src/Components/PaginationControls.tsx
@@ -5,24 +5,21 @@ import { FC } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 interface PaginationControlsProps {
-    hasNextPage: boolean;
-    hasPrevPage: boolean;
     dataLength: number;
 }
 
-const PaginationControls: FC<PaginationControlsProps> = ({ hasNextPage, hasPrevPage, dataLength }) => {
+const PaginationControls: FC<PaginationControlsProps> = ({ dataLength }) => {
     const router = useRouter();
     const searchParams = useSearchParams();
 
     const page = searchParams.get("page") ?? "1";
     const per_page = searchParams.get("per_page") ?? "5";
-
     return (
         <div>
             <button
                 type="button"
-                disabled={!hasPrevPage}
-                onClick={() => router.push(`/?page=${Number(page) - 1}&per_page=${per_page}`, { scroll: false })}
+                disabled={!(+page > 1)}
+                onClick={() => router.push(`http://localhost:3000/?page=${Number(page) - 1}&per_page=${per_page}`, { scroll: false })}
             >
                 prev page
             </button>
@@ -32,8 +29,8 @@ const PaginationControls: FC<PaginationControlsProps> = ({ hasNextPage, hasPrevP
 
             <button
                 type="button"
-                disabled={!hasNextPage}
-                onClick={() => router.push(`/?page=${Number(page) + 1}&per_page=${per_page}`, { scroll: false })}
+                disabled={!(+page * +per_page < dataLength)}
+                onClick={() => router.push(`http://localhost:3000/?page=${Number(page) + 1}&per_page=${per_page}`, { scroll: false })}
             >
                 next page
             </button>
--- src/Components/PaginationExample.tsx
+++ src/Components/PaginationExample.tsx
@@ -7,25 +7,13 @@ interface PaginationExampleProps {
 
 const PaginationExample: React.FC<PaginationExampleProps> = async ({ searchParams }) => {
     const entries = (await postResourse("http://localhost:3000/api/load-entry", searchParams)) as IEntry[] | [] | undefined;
-    const entriesLength = await getResourse("http://localhost:3000/api/count-entries");
-    const page = searchParams["page"] ?? "1";
-    const per_page = searchParams["per_page"] ?? "5";
-    // console.log(entries, entriesLength);
-
-    /* const { isLoading, error, data } = useQuery<IEntry[] | undefined>({
-        queryKey: ["entryData"],
-        queryFn: () => getResourse("https://api.github.com/repos/TanStack/query"),
-    });
-
-    if (isLoading) return "Loading...";
-    if (error instanceof Error) return "An error has occured: " + error.message; */
+    const entriesLength = (await getResourse("http://localhost:3000/api/count-entries")) as number;
 
     return (
         <div>
             {entries && entries.map(each => <p key={each.id}>{each.data}</p>)}
 
-            <PaginationControls hasNextPage={+page * +per_page < entriesLength} hasPrevPage={+page > 1} dataLength={entriesLength} />
-            {/* <button onClick={fetchdata}>on CLick</button> */}
+            <PaginationControls dataLength={entriesLength} />
         </div>
     );
 };
